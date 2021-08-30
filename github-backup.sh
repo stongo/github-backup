d54efbbdbd222f0808307834fc834c13188677a8
Markus Lenger <markus.lenger@gmail.com>
2021-08-30 12:05:37 +0200

    Using mktemp to create temporary directory

--- github-backup.sh
+++ github-backup.sh
@@ -12,10 +12,9 @@ USER="$2"
 AUTHENTICATION_API_TOKEN="${GITHUB_AUTH_TOKEN:?"must be set and non-empty"}:x-oauth-basic"
 REPOS_API_URL="https://api.github.com/orgs/${ORG}/repos?type=owner"
 DATE=$(date +"%Y%m%d")
-TEMP_DIR="github_${ORG}_${DATE}"
-BACKUP_FILE="${TEMP_DIR}.tgz"
-
-mkdir "$TEMP_DIR" && cd "$TEMP_DIR"
+TEMP_DIR=`mktemp -d`
+BACKUP_FILE="github_${ORG}_${DATE}.tgz"
+cd "$TEMP_DIR"
 curl -u $AUTHENTICATION_API_TOKEN -s "$REPOS_API_URL" | grep -Eo '"clone_url": "[^"]+"' |\
  awk '{print $2}' | sed s"#\"https://#\"https://$USER:$GITHUB_AUTH_TOKEN@#" | xargs -n 1 git clone --mirror
 cd -
