acfa1c9e99ce083b0255f4260a50fec522950fe3
Markus Lenger <markus.lenger@gmail.com>
2021-09-17 14:34:33 +0200

    Removed obsolute user-argument

--- github-backup.sh
+++ github-backup.sh
@@ -17,14 +17,13 @@ on_exit()
 trap on_exit EXIT
 
 ORG="$1"
-USER="$2"
 AUTHENTICATION_API_TOKEN="${GITHUB_AUTH_TOKEN:?"must be set and non-empty"}:x-oauth-basic"
 REPOS_API_URL="https://api.github.com/orgs/${ORG}/repos?type=owner"
 DATE=$(date +"%Y%m%d")
 TEMP_DIR=`mktemp -d`
-BACKUP_FILE="github_${ORG}_${DATE}.tgz"
+BACKUP_FILE="`pwd`/github_${ORG}_${DATE}.tgz"
 cd "$TEMP_DIR"
 curl -u $AUTHENTICATION_API_TOKEN -s "$REPOS_API_URL" | grep -Eo '"clone_url": "[^"]+"' |\
- awk '{print $2}' | sed s"#\"https://#\"https://$USER:$GITHUB_AUTH_TOKEN@#" | xargs -n 1 git clone --mirror
-cd -
+ awk '{print $2}' | sed s"#\"https://#\"https://$GITHUB_AUTH_TOKEN@#" | xargs -n 1 git clone --mirror
+
 tar zcf "$BACKUP_FILE" "$TEMP_DIR"
